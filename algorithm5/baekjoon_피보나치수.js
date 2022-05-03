b7b83192c4e43589ea3b18c399e23ff7350a8cde
bongjin <poseson92@naver.com>
2022-05-03 12:22:37 +0900

    [알고리즘]

--- algorithm5/baekjoon_피보나치수.js
+++ algorithm5/baekjoon_피보나치수.js
@@ -1,10 +1,11 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split(" ");
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split(' ');
 
-const num = +input;
-const fibo = [
-  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765,
-];
+let fibo = [0, 1];
 
-console.log(fibo[num]);
+for (let i = 2; i <= Number(input); i++) {
+  fibo.push(fibo[i - 1] + fibo[i - 2]);
+}
+
+console.log(fibo[Number(input)]);
